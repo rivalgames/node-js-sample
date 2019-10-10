9d0cbe565ff2c070675df6fe4a13b186be049465
Guy Margalit <guy.margalit0@gmail.com>
2019-10-10 11:40:00 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express')
 var app = express()
+var cors = require('cors')
+
+app.use(cors())
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
+})
+
+app.post('/login', function(request, response) {
+  response.send('Invalid username/password')
 })
 
 app.listen(app.get('port'), function() {
